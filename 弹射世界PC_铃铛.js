c90f2b09563f7ee5284c5195e2db961698d8255a
IDLER <59228469+UbiquitousAsh@users.noreply.github.com>
2023-02-14 12:59:36 +0800

    Add files via upload

--- 弹射世界PC_铃铛.js
+++ 弹射世界PC_铃铛.js
@@ -17,9 +17,15 @@ var isSettling;
 //铃铛坐标46，34.5
 var LD_x = x * 0.065; //35
 var LD_y = y * 0.027; //26
-//点击参加 
-var CJ_x = x * 0.518; //280
-var CJ_y = y * 0.104; //100
+//点击参加 1
+var CJ_x1 = x * 0.518; //280
+var CJ_y1 = y * 0.072; //70
+//点击参加 2
+var CJ_x2 = x * 0.518; //280
+var CJ_y2 = y * 0.135; //130
+//点击参加 3
+var CJ_x3 = x * 0.518; //280
+var CJ_y3 = y * 0.197; //190
 //失败按钮点击
 var FailCJ_x = x * 0.597; //
 var FailCJ_y = y * 0.633; //
@@ -43,7 +49,7 @@ var Login_y = y * 0.976;
 var Selltement_x = x * 0.416;
 var Selltement_y = y * 0.937;
 //升级
-var LevelUp_x = x * 0.33; // 180 （540）
+var LevelUp_x = x * 0.24; // 130 （540）
 var LevelUp_y = y * 0.505; // 485 （960）
 //新角色
 var Character_x = x * 0.277; //150 （540）
@@ -54,18 +60,40 @@ var LD_Green_y = y * 0.0234;
 //登录领取每日奖励
 var activity_x = x * 0.375; //
 var activity_y = y * 0.914; //
+
+
+// 颜色
+var white = "#ffffffff"; //白
+var cj_green = "#ff29c2b5"; //参加绿
+var zb_gray = "#ffe7e3e7"; // 准备灰
+var js_green = "#ffdedbde" //结算绿
+var js_blue = "#ff002c4a" //获得角色蓝
+var sj_white = "#fff7f7f7" //升级白
+var zy_color = "#ffded3ce" //主页判断色
+var xs_orange = "#ffff9f1c" //新手橙
+
 while (1) {
     var img = captureScreen();
     var color = images.pixel(img, LD_x, LD_y);
-    if (colors.isSimilar(color, colors.parseColor("#ffffffff"))) {//有铃铛
+    if (colors.isSimilar(color, colors.parseColor(white))) {//有铃铛
         //点击小铃铛
         click(LD_x, LD_y);
         toast("参加救援");
         sleep(1000);
 
         //点击参加
-        if (colors.isSimilar(images.pixel(captureScreen(), CJ_x, CJ_y), colors.parseColor("#ff29c2b5"))) {
-            click(CJ_x, CJ_y);
+        if (colors.isSimilar(images.pixel(captureScreen(), CJ_x1, CJ_y1), colors.parseColor(cj_green)) || colors.isSimilar(images.pixel(captureScreen(), CJ_x1, CJ_y1), colors.parseColor(xs_orange))) {
+            click(CJ_x1, CJ_y1);
+            toast("点击参加");
+            sleep(1000);
+        }
+        else if(colors.isSimilar(images.pixel(captureScreen(), CJ_x2, CJ_y2), colors.parseColor(cj_green)) || colors.isSimilar(images.pixel(captureScreen(), CJ_x2, CJ_y2), colors.parseColor(xs_orange))){
+            click(CJ_x2, CJ_y2);
+            toast("点击参加");
+            sleep(1000);
+        }
+        else if(colors.isSimilar(images.pixel(captureScreen(), CJ_x3, CJ_y3), colors.parseColor(cj_green)) || colors.isSimilar(images.pixel(captureScreen(), CJ_x3, CJ_y3), colors.parseColor(xs_orange))){
+            click(CJ_x3, CJ_y3);
             toast("点击参加");
             sleep(1000);
         }
@@ -74,7 +102,7 @@ while (1) {
         }
 
         //人满失败
-        if (colors.isSimilar(images.pixel(captureScreen(), FailCJ_x, FailCJ_y), colors.parseColor("#ff29c2b5"))) {
+        if (colors.isSimilar(images.pixel(captureScreen(), FailCJ_x, FailCJ_y), colors.parseColor(cj_green))) {
             click(FailCJ_x, FailCJ_y);
             toast("参加失败，人满");
             sleep(3000);
@@ -93,15 +121,15 @@ while (1) {
             isDissolution = images.pixel(WaitingImg, FailCJ_x, FailCJ_y);
             Ready = images.pixel(WaitingImg, ZB_x, ZB_y);
 
-            if (colors.isSimilar(WaitingFight, colors.parseColor("#ffffffff"), 8)) {
+            if (colors.isSimilar(WaitingFight, colors.parseColor(white), 8)) {
                 toast("开打");
                 break;
             }
-            if (colors.isSimilar(Ready, colors.parseColor("#ffe7e3e7"), 8)) {
+            if (colors.isSimilar(Ready, colors.parseColor(zb_gray), 8)) {
                 click(ZB_x, ZB_y);
                 toast("准备");
             }
-            if (colors.isSimilar(isDissolution, colors.parseColor("#ff29c2b5"))) {
+            if (colors.isSimilar(isDissolution, colors.parseColor(cj_green))) {
                 click(FailCJ_x, FailCJ_y);
                 toast("队伍人满/解散");
                 break;
@@ -110,7 +138,7 @@ while (1) {
             sleep(5000);
         }
         //失败，继续等待铃铛
-        if (colors.isSimilar(isDissolution, colors.parseColor("#ff29c2b5"))) {
+        if (colors.isSimilar(isDissolution, colors.parseColor(cj_green))) {
             continue;
         }
 
@@ -129,12 +157,12 @@ while (1) {
             Finish = images.pixel(image, LD_x, LD_y);
             Finish_Green = images.pixel(image, LD_Green_x, LD_Green_y);
             Character = images.pixel(image, Character_x, Character_y);
-            if (colors.isSimilar(Finish, colors.parseColor("#ffdedbde"), 8) || (colors.isSimilar(Finish, colors.parseColor("#ffffffff")) && colors.isSimilar(Finish_Green, colors.parseColor("#ff29c2b5")))) {
+            if (colors.isSimilar(Finish, colors.parseColor(js_green), 8) || (colors.isSimilar(Finish, colors.parseColor(white)) && colors.isSimilar(Finish_Green, colors.parseColor(cj_green)))) {
                 toast("结算结束");
                 isSettling = 0;
                 break;
             }
-            else if (colors.isSimilar(Selltement, colors.parseColor("#ff29c2b5"))) {
+            else if (colors.isSimilar(Selltement, colors.parseColor(cj_green))) {
                 //结算中
                 click(Selltement_x, Selltement_y);
                 isSettling = 1;
@@ -145,19 +173,19 @@ while (1) {
                 toast("等待结算");
                 sleep(3000);
             }
-            else if (colors.isSimilar(Character, colors.parseColor("#ff002c4a")) || colors.isSimilar(Character, colors.parseColor("#ffffffff"), 8)) {
+            else if (colors.isSimilar(Character, colors.parseColor(js_blue)) || colors.isSimilar(Character, colors.parseColor(white), 8)) {
                 click(Selltement_x, Selltement_y);
                 toast("获得角色");
                 sleep(3000);
                 continue;
             }
-            else if (colors.isSimilar(TooLong, colors.parseColor("#ff29c2b5"))) {
+            else if (colors.isSimilar(TooLong, colors.parseColor(cj_green))) {
                 click(FailCJ_x, FailCJ_y);
                 toast("太久了,结算结束 || 日期改变");
                 sleep(3000);
                 break;
             }
-            else if (colors.isSimilar(LevelUp, colors.parseColor("#fff7f7f7"), 7)) {//升级了
+            else if (colors.isSimilar(LevelUp, colors.parseColor(sj_white), 7)) {//升级了
                 click(Selltement_x, Selltement_y);
                 toast("Level up");
                 sleep(1000);
@@ -165,12 +193,12 @@ while (1) {
             }
         }
     }
-    else if (colors.isSimilar(images.pixel(img, Date_x, Date_y), colors.parseColor("#ff29c2b5"))) {//日期改变
+    else if (colors.isSimilar(images.pixel(img, Date_x, Date_y), colors.parseColor(cj_green))) {//日期改变
         click(Date_x, Date_y);
         toast("日期改变");
         sleep(10000);
     }
-    else if (colors.isSimilar(images.pixel(img, Login_x, Login_y), colors.parseColor("#ffded3ce"), 8)) {//检测是否到登录界面
+    else if (colors.isSimilar(images.pixel(img, Login_x, Login_y), colors.parseColor(zy_color), 8)) {//检测是否到登录界面
         click(Login_x, Login_y);
         toast("点击开始进入游戏");
         sleep(10000);
@@ -186,13 +214,13 @@ while (1) {
                 sleep(1000);//进入游戏大概率卡顿，导致画面出来，却无法点击，等待1秒以确保点上按钮
                 click(Continue_x, Continue_y);
             }
-            else if(colors.isSimilar(color, colors.parseColor("#ffdedbde"), 8)){//不需要领取登录奖励 / 领取完登录奖励
+            else if (colors.isSimilar(color, colors.parseColor(js_green), 8)) {//不需要领取登录奖励 / 领取完登录奖励
                 break;
             }
-            else{
+            else {
                 //关闭公告
-                    click(activity_x, activity_y);
-                    toast("点击关闭公告" + colors.toString(color));
+                click(activity_x, activity_y);
+                toast("点击关闭公告" + colors.toString(color));
             }
             sleep(3000);
         }
